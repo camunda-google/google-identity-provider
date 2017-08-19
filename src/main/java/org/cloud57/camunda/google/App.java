4859c5c14267f240b59e90b911511c31fe9a9c52
CloudyDirk <d.guenther@cloud57.de>
2017-08-19 21:25:10 +0200

    Maven added

---
+++ src/main/java/org/cloud57/camunda/google/App.java
@@ -0,0 +1,13 @@
+package org.cloud57.camunda.google;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
